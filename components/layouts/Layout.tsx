715dcb123ddc72ab56fa318a1049becbc9f0a1c7
Andrey Emelianov <621077@mail.ru>
2024-03-19 14:45:36 +0300

    реализовал компонент NavbarMobile отображающий навигацию при разрешении меньше 800px стилизовал, добавил иконки

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,13 +1,21 @@
+'use client'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
 import { Header } from '../modules/Header/Header'
+import { NavbarMobile } from '../modules/NavbarMobile/NavbarMobile'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
-export const Layout = ({ children }: LayoutProps) => (
-  <>
-    <Header />
-    {children}
-    <div>footer</div>
-  </>
-)
+export const Layout = ({ children }: LayoutProps) => {
+  const isMedia800 = useMediaQuery(800)
+
+  return (
+    <>
+      <Header />
+      {children}
+      {isMedia800 && <NavbarMobile />}
+      <div>footer</div>
+    </>
+  )
+}
