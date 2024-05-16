a4301b69a6987dd1a0b917ab58006b01a3e204a5
Andrey Emelianov <621077@mail.ru>
2024-05-16 20:36:14 +0300

    подключил axios,  создал instanceAxios, реализовал две функции эффекта на получение по созданным роутам новых товаров и бестселлеров, в файле goods.ts  создал домен и гейт через эффектор, реализовал стор с товарами, создал sample и передал в него эффекты и гейт

--- components/templates/MainPage/MainPage.tsx
+++ components/templates/MainPage/MainPage.tsx
@@ -1,9 +1,16 @@
+import { useGate } from 'effector-react'
+
 import { Categories } from '@/components/modules/MainPage/Categories/Categories'
 import { Hero } from '@/components/modules/MainPage/Hero/Hero'
+import { MainPageGate } from '@/context/goods'
 
-export const MainPage = () => (
-  <main>
-    <Hero />
-    <Categories />
-  </main>
-)
+export const MainPage = () => {
+  useGate(MainPageGate)
+
+  return (
+    <main>
+      <Hero />
+      <Categories />
+    </main>
+  )
+}
