5a87a53838a3841958c1dce96d5c7a7265443c1f
Andrey Emelianov <621077@mail.ru>
2024-06-10 18:40:00 +0300

    реализовал ещё два эндпоинта на сервере, для удаления конкретного товара из админки и с бд, и удалению нескольких товаров из админки и бд

---
+++ app/api/admin/goods/delete-many/route.ts
@@ -0,0 +1,50 @@
+import { ObjectId } from 'mongodb'
+import { NextResponse } from 'next/server'
+
+import { corsHeaders } from '@/constants/corsHeaders'
+import { clientPromise } from '@/lib/mongodb'
+import { getDbAndReqBody } from '@/lib/utils/api-routes'
+
+export async function GET(req: Request) {
+  try {
+    const { db } = await getDbAndReqBody(clientPromise, null)
+
+    const url = new URL(req.url)
+
+    const ids = url.searchParams.get('ids')
+
+    if (!ids) {
+      return NextResponse.json(
+        {
+          status: 404,
+          message: 'Ids are required',
+        },
+        corsHeaders
+      )
+    }
+
+    const parsedId = JSON.parse(ids) as string[]
+
+    const deleteManyFromCollection = async (collection: string) => {
+      await db.collection(collection).deleteMany({
+        _id: {
+          $in: parsedId.map((id) => new ObjectId(id)),
+        },
+      })
+    }
+
+    await Promise.allSettled([
+      deleteManyFromCollection('cloth'),
+      deleteManyFromCollection('accessories'),
+    ])
+
+    return NextResponse.json(
+      {
+        status: 204,
+      },
+      corsHeaders
+    )
+  } catch (error) {
+    throw new Error((error as Error).message)
+  }
+}
